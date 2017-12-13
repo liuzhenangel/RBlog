8254714401f42cec05d7c9946852ba4e33d265e3
liuzhenangel <550686383@qq.com>
2017-12-13 11:06:20 +0800

    update config

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,5 @@
 if ENV['RAILS_ENV'] == 'production'
-  app_root = '/data/www/RBlog/shared'
+  app_root = '/home/ruby/RBlog/shared'
   pidfile "#{app_root}/tmp/pids/puma.pid"
   state_path "#{app_root}/tmp/pids/puma.state"
   bind "unix://#{app_root}/tmp/sockets/puma.sock"
